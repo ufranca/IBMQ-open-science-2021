c745011c3c21a8095ff8ddca2b26c6cbf69f572b
Urbano Franca <urbano.franca@gmail.com>
2022-05-02 21:41:56 -0400

    few tweaks and new main fig

---
+++ jakarta_runs_pe_gates.py
@@ -0,0 +1,119 @@
+
+
+###############################################
+## Jobs - Pulse-efficient template optimized
+###############################################
+
+#Trotter steps - n=5
+list_jobs_pe_n5_run1 = ['62702134a46375b634628d54',
+                        '62702136a463750d69628d55',
+                        '627021388b4cc3e8f6c8d916',
+                        '6270213b0ed02941f78f8cb8',
+                        '6270213de560441f71f941f9',
+                        '6270213fae25c1577de95786',
+                        '62702141171ee8735c256ebb',
+                        '627021493069f6ade03eb922']
+
+#Trotter steps - n=6
+list_jobs_pe_n6_run1 = ['627002a33069f609bc3eb8cf',
+                        '627002a68d1838226ca4d245',
+                        '627002a9e56044cb48f941a7',
+                        '627002ab0ed0299a258f8c79',
+                        '627002adae25c13a69e95734',
+                        '627002af8d1838ae8aa4d246',
+                        '627002b28d1838049ba4d247',
+                        '627002b4e56044b863f941a8'
+                       ]
+
+#Trotter steps - n=7
+list_jobs_pe_n7_run1 = ['626fce618b4cc368c7c8d818',
+                        '626fce6430321157f40f94df',
+                        '626fce67a463755a80628c45',
+                        '626fce69171ee86586256da3',
+                        '626fce6cae25c128dfe9566c',
+                        '626fce6e171ee8dc42256da4',
+                        '626fce71e5604462fdf940f4',
+                        '626fce743069f6534e3eb812'
+                       ]
+
+list_jobs_pe_n7_run2 = ['627032a1ae25c13ad4e957d1',
+						'627032a48b4cc360a7c8d953',
+						'627032a7171ee860f9256f17',
+						'627032aa3069f61b053eb975',
+						'627032aca46375ebea628db0',
+						'627032afae25c1b353e957d2',
+						'627032b1e560443ec2f9423d',
+						'627032b48b4cc39929c8d954']
+
+
+#Trotter steps - n=8
+list_jobs_pe_n8_run1 = ['625b5ae0f1fb8d40af3ad927',
+                        '625b5ae4e1f245f2cdeb6ce4',
+                        '625b5ae90d8f19451bac1aaa',
+                        '625b5aed9e311582cf838b50',
+                        '625b5af13bbc9fb061735c4e',
+                        '625b5af5736894b9c6c8bce7',
+                        '625b5af98e4b23cb72d30a36',
+                        '625b5afda7e9666acddee870']
+
+
+list_jobs_pe_n8_run2 = ['626fbf01ae25c109d3e9565c',
+                        '626fbf043032113fbe0f94ca',
+                        '626fbf070ed029218c8f8bcb',
+                        '626fbf0a8b4cc36470c8d801',
+                        '626fbf0c8d18382ce4a4d178',
+                        '626fbf0f3069f62bfe3eb804',
+                        '626fbf12a46375da48628c2b',
+                        '626fbf150ed0299a6e8f8bcc']
+
+#Trotter steps - n=9
+list_jobs_pe_n9_run1 = ['626fce7fbb2ed332753195c9',
+                        '626fce82171ee8cce7256da5',
+                        '626fce85a463758024628c46',
+                        '626fce888b4cc34221c8d81a',
+                        '626fce8b3069f6dfb73eb813',
+                        '626fce8ebb2ed3219c3195ca',
+                        '626fce91e56044df39f940f7',
+                        '626fce93303211159f0f94e0']
+
+#Trotter steps - n=10
+list_jobs_pe_n10_run1 = ['62702bc13069f6291d3eb946',
+                        '62702bc4ae25c185ace957a2',
+                        '62702bc83069f643043eb947',
+                        '62702bcb0ed02911938f8cdc',
+                        '62702bce3069f638c83eb948',
+                        '62702bd28d18384948a4d2b6',
+                        '62702bd5303211543c0f9602',
+                        '62702bd83069f62be03eb949']
+
+#Trotter steps - n=11
+list_jobs_pe_n11_run1 = ['625b5cc473689461f9c8bced',
+                        '625b5cca8e4b231934d30a38',
+                        '625b5cce35e1ec41d9253740',
+                        '625b5cd3f1fb8d67323ad92d',
+                        '625b5cd87368941e87c8bcee',
+                        '625b5cdda7e96664addee873',
+                        '625b5ce20d8f1980abac1aaf',
+                        '625b5ce7e3b67fc84a3abd81']
+
+list_jobs_pe_n11_run2 = ['626fbafc3069f6b7f23eb7fa',
+                        '626fbb003069f60bf43eb7fb',
+                        '626fbb04a46375a864628c21',
+                        '626fbb07171ee8d999256d84',
+                        '626fbb0a8d18385481a4d170',
+                        '626fbb0f3069f61ee13eb7fc',
+                        '626fbb13ae25c10a54e95650',
+                        '626fbb163069f6f3fc3eb7fd']
+
+
+#Trotter steps - n=12
+list_jobs_pe_n12_run1 = ['627034f8bb2ed3139131971f',
+                        '627034fde56044fc4df94243',
+                        '62703500ae25c123ece957d8',
+                        '62703504bb2ed345bc319720',
+                        '62703508bb2ed3fcb4319721',
+                        '6270350b0ed0292d748f8d0b',
+                        '6270350e8d1838cd28a4d2e9',
+                        '627035133032117be10f9642']
+
+
