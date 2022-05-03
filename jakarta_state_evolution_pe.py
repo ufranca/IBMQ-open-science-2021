6e7bfdc359d23e5bf188128020c96a42058b5c11
Urbano Franca <urbano.franca@gmail.com>
2022-05-03 16:01:53 -0400

    fixing typos

---
+++ jakarta_state_evolution_pe.py
@@ -0,0 +1,17 @@
+## Time t0 = 0
+list_jobs_pe_state_t0_run1 =['6270431cae25c1e5b0e957f2', '6270433abb2ed315a0319742']
+
+## Time t0 = 0
+list_jobs_pe_state_t0 =['6270454aa463759484628ddc', '627045618d1838fab4a4d312']
+
+## Time t1 = (1/4)*np.pi
+list_jobs_pe_state_t1 =['6270469ba46375836b628de1', '627046ab171ee89679256f40']
+
+## Time t2 = (2/4)*np.pi
+list_jobs_pe_state_t2 =['627047e53069f626243eb9a6', '627047f58b4cc3c90ac8d980']
+
+## Time t3 = (3/4)*np.pi
+list_jobs_pe_state_t3 =['6270492f3069f6feb63eb9a9', '6270493fe560441b2bf94268']
+
+## Time t4 = (4/4)*np.pi
+list_jobs_pe_state_t4 =['62704a7a8d1838bfeaa4d317','62704a93e560441d12f9426a']
